ee90da13b65a1707340b05766c295e6eecbc70cb
Anton Fenske <anton@alvalabs.io>
2023-02-01 21:19:27 +0000

    Integrate with SQLite

--- app/src/main/kotlin/co/devskills/kotlinboilerplate/PingController.kt
+++ app/src/main/kotlin/co/devskills/kotlinboilerplate/PingController.kt
@@ -3,11 +3,25 @@ package co.devskills.kotlinboilerplate
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
+import java.sql.Connection
+import java.sql.DriverManager
+
 @RestController
 class PingController {
 
     @GetMapping("/ping")
     fun ping(): String {
-        return "pong"
+        var conn: Connection? = null
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:appdb.sqlite")
+            val statement = conn.createStatement()
+            val result = statement.executeQuery("SELECT 1")
+            result.next()
+            return result.getString(1)
+        } catch (e: Exception) {
+            return "Error connecting to database: ${e.message}"
+        } finally {
+            conn?.close()
+        }
     }
 }
